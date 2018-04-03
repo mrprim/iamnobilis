717bf36f270a0c86afd0f5f4cf89bd83bd51fecb
Sergio Rodriguez <oneandprime@gmail.com>
2018-04-02 23:41:47 -0400

    Fixing some issues

--- app/components/Character/index.js
+++ app/components/Character/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './index.scss'
 
 const Character = ({ identity, descriptor, estate, generateCharacterClick }) => (
-  <div onClick={generateCharacterClick}>
-    I am the {identity} who is the {descriptor} god of {estate}.
+  <div className='character' onClick={generateCharacterClick}>
+    {'I am the '}
+    <span className='modifier identity'>
+      {identity}
+    </span>
+    {' who is the '}
+    <span className='modifier descriptor'>
+      {descriptor}
+    </span>
+    {' god of '}
+    <span className='modifier estate'>
+      {estate}
+    </span>
+    {'.'}
   </div>
 )
 
