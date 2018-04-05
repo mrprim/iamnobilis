fa7854f218d76dcb14ebe5e354c395901ef24ff1
Sergio Rodriguez <oneandprime@gmail.com>
2018-04-04 22:50:44 -0400

    Fixed some randomization stuff

--- app/sagas/generateCharacter.js
+++ app/sagas/generateCharacter.js
@@ -3,6 +3,7 @@ import { takeEvery, put } from 'redux-saga/effects'
 import * as types from '../constants/actionTypes'
 import * as actions from '../actions/character'
 import generators from 'random-rpg-stuff'
+import sample from '../utils/sample'
 
 export default function * () {
   yield takeEvery(types.GENERATE_CHARACTER, loader)
@@ -12,9 +13,17 @@ export function * loader (action) {
   let i = 0
   while (i < 8) {
     const char = {
-      identity: generators.things.random(),
-      descriptor: generators.things.random(),
-      estate: generators.things.random()
+      identity: sample([
+        generators.things.animals(),
+        generators.characters.classes()
+      ]),
+      descriptor: generators.misc.descriptors(),
+      estate: sample([
+        generators.things.random(),
+        generators.things.random(),
+        generators.things.random(),
+        generators.characters.classes()
+      ])
     }
     yield put(actions.setCharacter(char))
     yield delay(50)
