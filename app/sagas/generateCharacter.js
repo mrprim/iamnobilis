6acd08e30fde282455ddef7a667604938fd13cb6
Sergio Rodriguez <oneandprime@gmail.com>
2018-04-06 21:44:26 -0400

    Added random nouns and upgraded random-rpg-stuff

--- app/sagas/generateCharacter.js
+++ app/sagas/generateCharacter.js
@@ -2,8 +2,7 @@ import { delay } from 'redux-saga'
 import { takeEvery, put } from 'redux-saga/effects'
 import * as types from '../constants/actionTypes'
 import * as actions from '../actions/character'
-import generators from 'random-rpg-stuff'
-import sample from '../utils/sample'
+import randomRpgStuff from 'random-rpg-stuff'
 import pluralize from 'pluralize'
 
 export default function * () {
@@ -15,17 +14,9 @@ export function * loader (action) {
   yield put(actions.setLoading(true))
   while (i < 8) {
     const char = {
-      identity: sample([
-        generators.things.animals(),
-        generators.characters.classes()
-      ]),
-      adjective: generators.misc.descriptors(),
-      estate: pluralize(sample([
-        generators.things.random(),
-        generators.things.random(),
-        generators.things.random(),
-        generators.characters.classes()
-      ]))
+      identity: randomRpgStuff(['animal', 'class']),
+      adjective: randomRpgStuff(['descriptor']),
+      estate: pluralize(randomRpgStuff('noun'))
     }
     yield put(actions.setCharacter(char))
     yield delay(50)
