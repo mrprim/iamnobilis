70d54f766475c6da2fe32784322cdb6f387fe358
Sergio Rodriguez <oneandprime@gmail.com>
2018-04-07 21:33:09 -0400

    Fixed the copy functionality

--- app/utils/getCharacterString.js
+++ app/utils/getCharacterString.js
@@ -1,12 +1,30 @@
 import aVsAn from './aVsAn'
 
 const getCharacterString = character => {
+  switch (character.type) {
+    case 'noble': return noble(character)
+    case 'strategist': return strategist(character)
+    default: return noble(character)
+  }
+}
+
+const noble = character => {
   return 'I am ' +
     aVsAn(character.identity) + ' ' +
     character.identity.toLowerCase() +
     ' who is the ' +
     character.adjective +
     ' god of ' +
+    character.estate.toLowerCase()
+}
+
+const strategist = character => {
+  return 'I am ' +
+    aVsAn(character.identity) + ' ' +
+    character.identity.toLowerCase() +
+    ' who is the ' +
+    character.adjective +
+    ' Strategist dying from ' +
     character.estate.toLowerCase()
 }
 
