5d8284b5858173966b16f9f4d872e7699883ac43
Sergio Rodriguez <oneandprime@gmail.com>
2018-08-01 22:56:34 -0400

    Gzipping responses

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const compression = require('compression')
 
+app.use(compression())
 app.use(express.static('./build'))
 app.listen(process.env.PORT || 3000, () => console.log('iamnobilis listening on port ' + (process.env.PORT || 3000)))
