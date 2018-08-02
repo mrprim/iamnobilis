ce071ae8f0faf8a1ca6fe3f2cce2e00041188f98
Sergio Rodriguez <oneandprime@gmail.com>
2018-08-01 23:29:03 -0400

    Add http2 Support

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const compression = require('compression')
+const http2 = require('http2')
 
 app.use(compression())
 app.use(express.static('./build'))
-app.listen(process.env.PORT || 3000, () => console.log('iamnobilis listening on port ' + (process.env.PORT || 3000)))
+http2.createServer(app).listen(process.env.PORT || 3000, () => console.log('iamnobilis listening on port ' + (process.env.PORT || 3000)))
