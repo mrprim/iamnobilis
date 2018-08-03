7b4ca4f894ee133ff017d5a3c5245807970cb5d1
Sergio Rodriguez <oneandprime@gmail.com>
2018-08-02 21:00:56 -0400

    Setting up SSL

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const compression = require('compression')
 
 app.use(compression())
 app.use(express.static('./build'))
+app.get('/.well-known/acme-challenge/:content', function (req, res) {
+  res.send('fQ7GJvG6pieSx6hbDWnT0Sz0vNT0QFm4nW91ZrtnefA.FDcoEvHYy21hfSj3-5YKjt6HD2Ui3j3hG4M_BpAKByc')
+})
+
 app.listen(process.env.PORT || 3000, () => console.log('iamnobilis listening on port ' + (process.env.PORT || 3000)))
