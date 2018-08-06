67bea6fc5e94222a2c54e3399503015f2cb8ef26
Sergio Rodriguez <oneandprime@gmail.com>
2018-08-05 22:08:56 -0400

    Made some modifications

--- app/index.js
+++ app/index.js
@@ -1,42 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import Layout from './containers/Layout'
 import configureStore from './store'
 import startSagas from './sagas'
 import routes from './routes'
+import registerServiceWorker from './utils/registerServiceWorker'
 
 export const store = configureStore()
 
 startSagas()
+registerServiceWorker('/sw.bundle.js')
 
 const mapRouteToRouteComponent = r => <Route {...r} key={r.path} />
 
 ReactDOM.render(<div>
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <Layout>
         <Switch>
           {routes.map(mapRouteToRouteComponent)}
         </Switch>
       </Layout>
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 </div>, document.getElementById('app'))
-
-const registerServiceWorker = () => {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.register('/sw.bundle.js').then(function (registration) {
-      // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope)
-      }, function (err) {
-      // registration failed :(
-        console.log('ServiceWorker registration failed: ', err)
-      })
-    })
-  }
-}
-
-registerServiceWorker()
